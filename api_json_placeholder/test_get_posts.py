88ce7e2fd682b929ee69e56efbed8164d27df58c
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 15:18:41 +0000

    now properly sending the params and getting an expected fail on the test

--- api_json_placeholder/test_get_posts.py
+++ api_json_placeholder/test_get_posts.py
@@ -1,6 +1,7 @@
 import json
 from utils import get_urls
 from utils.json_placeholder_requests import get_request
+
 POSTS_URL = get_urls.ALL_POSTS
 
 
@@ -9,11 +10,11 @@ def test_get_all_posts_status_200():
     list_of_posts = response.json()
     status_code = response.status_code
     assert status_code == 200
-    assert list_of_posts[0]["id"] == 1    # Checking if there's a post with an ID of 1
+    assert list_of_posts[0]["id"] == 1  # Checking if there's a post with an ID of 1
 
 
 def test_get_posts_from_one_user():
     payload = {"userId": 1}
     response = get_request(POSTS_URL, params=payload)
     data = response.json()
-    assert len(data) == 100         # According to documentation - the number of posts from user 1 are 100
+    assert len(data) == 10  # According to documentation - the number of posts from user 1 are 10
--- utils/json_placeholder_requests.py
+++ utils/json_placeholder_requests.py
@@ -2,7 +2,8 @@ from venv import logger
 
 import requests
 
-def get_request(url, verify=False, params=None):
+def get_request(url, verify=False, **kwargs):
     logger.info("Starting a GET request...")
-    response = requests.get(url, verify=verify)
+    response = requests.get(url, verify=verify, **kwargs)
+    logger.info(f"Req URL: {response.request.url}")
     return response
