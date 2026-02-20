53495207513188a4dcf2a97dd4b23d5f8224feaa
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 13:05:30 +0000

    cleaned up the first test

--- api_json_placeholder/test_get_posts.py
+++ api_json_placeholder/test_get_posts.py
@@ -7,10 +7,10 @@ POSTS_URL = get_urls.ALL_POSTS
 
 def test_get_all_posts_status_200():
     response = get_request(POSTS_URL)
+    list_of_posts = response.json()
     status_code = response.status_code
-    print(response.json())
-    print(status_code)
     assert status_code == 200
+    assert list_of_posts[0]["id"] == 1    # Checking if there's a post with an ID of 1
 
 
 def test_get_posts_from_one_user():
