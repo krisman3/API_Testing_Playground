ebfb4892dd80b7a0d36ab1c7688b61e32b2e545d
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 13:51:56 +0000

    started separating logic with config

--- utils/get_urls.py
+++ utils/get_urls.py
@@ -1,3 +1,6 @@
-BASE_URL = "https://jsonplaceholder.typicode.com/"
+import json
+from config.env_utils import load_config
+
+BASE_URL = load_config("qa")
 POSTS_ENDPOINT = "posts/"
 ALL_POSTS = BASE_URL + POSTS_ENDPOINT
