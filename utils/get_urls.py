944aa70989ae3074c9069977924279676ce835f0
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 13:58:31 +0000

    fixed the config loading

--- utils/get_urls.py
+++ utils/get_urls.py
@@ -1,5 +1,6 @@
 from config.env_utils import load_config
+curr_config = load_config("qa")
 
-BASE_URL = load_config("qa")
+BASE_URL = curr_config["base_url"]
 POSTS_ENDPOINT = "posts/"
 ALL_POSTS = BASE_URL + POSTS_ENDPOINT
