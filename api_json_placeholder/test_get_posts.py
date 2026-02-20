48136d745eb8b7057430a2f6e1aeb0f381dbec9e
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 12:39:27 +0000

    added test for status 200

--- api_json_placeholder/test_get_posts.py
+++ api_json_placeholder/test_get_posts.py
@@ -1,14 +1,17 @@
 import pytest
 import requests
 from utils import get_urls
+from utils.json_placeholder_requests import get_request
+POSTS_URL = get_urls.ALL_POSTS
 
 
-def test_get_all_posts():
-    print(get_urls.ALL_POSTS)
-    response = requests.get(get_urls.ALL_POSTS, verify=False)
-    data = response.json()
+def test_get_all_posts_status_200():
+    response = get_request(POSTS_URL)
     status_code = response.status_code
-    print(data)
+    print(response.json())
     print(status_code)
-
     assert status_code == 200
+
+
+def test_get_posts_from_one_user():
+    pass
