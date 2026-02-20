8a8c45006e67641b688244a11076cd9db437cb32
kristiyan <kristiyan.iliev@dxc.com>
2026-02-20 13:38:22 +0000

    added params to the get request (for filtering cases)

--- utils/json_placeholder_requests.py
+++ utils/json_placeholder_requests.py
@@ -2,7 +2,7 @@ from venv import logger
 
 import requests
 
-def get_request(url, verify=False):
+def get_request(url, verify=False, params=None):
     logger.info("Starting a GET request...")
     response = requests.get(url, verify=verify)
     return response
