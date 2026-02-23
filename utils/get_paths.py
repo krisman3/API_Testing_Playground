e32db2b7e39ecb77335b4b0eb10e6e92337bdc44
kristiyan <kristiyan.iliev@dxc.com>
2026-02-23 15:03:28 +0000

    changed the path of the csv file and removed an unnecessary import

--- utils/get_paths.py
+++ utils/get_paths.py
@@ -1,6 +1,4 @@
 from config.env_utils import load_config, BASE_DIR
-from utils.get_urls import BASE_URL
 
 curr_config = load_config("qa")
-
-csv_title_file = BASE_DIR / "test_data" / "title_data.csv"
+csv_title_file = BASE_DIR / "test_data" / "title_data_positive.csv"
