902a86865161f9e5cd389ce67c09b8f8e6908cf4
kristiyan <kristiyan.iliev@dxc.com>
2026-02-24 08:24:09 +0000

    started get all posts from single user

--- api_json_placeholder/test_get_posts.py
+++ api_json_placeholder/test_get_posts.py
@@ -5,10 +5,13 @@ from utils.json_placeholder_requests import get_request
 from utils.read_csv import csv_reader
 from utils.get_paths import csv_title_file
 from utils.get_paths import id_body_file
+from utils.get_paths import number_data_file
 
 POSTS_URL = get_urls.ALL_POSTS
 title_csv_file = csv_reader(csv_title_file)
 id_csv_file = csv_reader(id_body_file)
+number_csv_file = csv_reader(number_data_file)
+
 
 def test_get_all_posts_status_200():
     response = get_request(POSTS_URL)
@@ -16,6 +19,13 @@ def test_get_all_posts_status_200():
     status_code = response.status_code
     assert status_code == 200
     assert list_of_posts[0]["id"] == 1  # Checking if there's a post with an ID of 1
+
+
+@pytest.mark.parametrize("userId,number,status", number_csv_file)
+def test_get_all_posts_from_single_user(userId, number, status):
+    payload = {"userId": userId}
+    response = get_request(POSTS_URL, params=payload)
+
 
 
 @pytest.mark.parametrize("id,body,status", id_csv_file)
