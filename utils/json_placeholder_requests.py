23c922e9897c09f7efa665a371e624a049b6ab2d
kristiyan <kristiyan.iliev@dxc.com>
2026-03-12 09:11:43 +0000

    added post request util

--- utils/json_placeholder_requests.py
+++ utils/json_placeholder_requests.py
@@ -7,5 +7,11 @@ import requests
 def get_request(url, verify=False, **kwargs):
     logger.info("Starting a GET request...")
     response = requests.get(url, verify=verify, **kwargs)
-    logger.info(f"Req URL: {response.request.url}")
+    return response
+
+
+def post_request(url, verify=False, **kwargs):
+    logger.info("Starting a POST request...")
+    response = requests.post(url, verify=verify, **kwargs)
+    print(f"Req URL: {response.request.headers}")
     return response
