01618229ba17e47194990fe1154423c611b15e29
kristiyan <kristiyan.iliev@dxc.com>
2026-03-24 14:08:54 +0000

    parametrized the single resource positive test

--- api_json_placeholder/test_post_posts.py
+++ api_json_placeholder/test_post_posts.py
@@ -1,9 +1,16 @@
 from utils.json_placeholder_requests import post_request
 from utils.get_urls import ALL_POSTS
+from utils.read_csv import csv_reader
+from utils.get_paths import creating_resources_file
+import pytest
 
-def test_single_resource_positive():
-    payload = {'title': 'RedBull', 'body': 'Max Verstappen #3', 'userId': 69}
+resources_file = csv_reader(creating_resources_file)
+
+@pytest.mark.parametrize("title,body,user_id", resources_file)
+def test_single_resource_positive(title,body,user_id):
+    payload = {'title': title, 'body': body, 'userId': int(user_id)}
     headers = {'Content-type': 'application/json', 'charset':'UTF-8'}
     response = post_request(params=payload, url=ALL_POSTS, headers=headers)
     assert response.status_code == 201
+
 
--- utils/get_paths.py
+++ utils/get_paths.py
@@ -4,3 +4,4 @@ curr_config = load_config("qa")
 csv_title_file = BASE_DIR / "test_data" / "title_data_positive.csv"
 id_body_file = BASE_DIR / "test_data" / "id_data_positive.csv"
 number_data_file = BASE_DIR / "test_data" / "number_posts_positive.csv"
+creating_resources_file = BASE_DIR / "test_data" / "creating_resources_positive.csv"
